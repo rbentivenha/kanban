88d17c3d3e2907186ac30a84a116d2c33f9afaeb
rbentivenha <bentivenha@gmail.com>
2020-08-10 15:53:51 -0300

    Added an unit test

--- kanban/activities/models.py
+++ kanban/activities/models.py
@@ -1,3 +1,15 @@
+# kanban.activitis.models
 from django.db import models
+import datetime
 
-# Create your models here.
+class Activity (models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    due_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+    def is_delayed(self):
+        return self.due_date < datetime.date.today()
--- kanban/activities/tests.py
+++ kanban/activities/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+from .models import Activity
+
+
+class TestActivity(TestCase):
+    def setUp(self):
+        Activity.objects.create(
+            name="Reunião de Negócios", description="Uma reunião muito importante", due_date="2020-08-08")
+    
+    def test_activities_is_delayed(self):
+        activity = Activity.objects.get(name="Reunião de Negócios")
+        self.assertEqual(activity.is_delayed(), True)
